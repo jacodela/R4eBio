98531b31a591de51ac10841ec99cec7f18fe5098
Jacobo de la Cuesta <jacobo.delacuesta@tuebingen.mpg.de>
2019-07-06 15:21:47 +0200

    Add wrangling notebook

--- 2019_Intro/Notebooks/Write_tables.R
+++ 2019_Intro/Notebooks/Write_tables.R
@@ -1,5 +1,6 @@
-# Get datasets from the dslabs package and save to files
+# Get datasets from the dslabs and ukbabynames packages and save to files
 library(dslabs)
+library(ukbabynames)
 library(dplyr)
 #Datasets
 data("murders") # Gun murder data for US states in 2012
@@ -8,9 +9,9 @@ data("us_contagious_diseases") # Yearly counts of several diseases for US states
 data("polls_us_election_2016") # Poll results from the US 2016 presidential elections
 data("heights") # Self-reported heights in inches for males and females
 data("divorce_margarine") # Spurious correlation data
-
+data("ukbabynames") # Spurious correlation data
 # Save tables
-tables_dir = "PATH_TO_FOLDER/2019_Data_Wrangling/Data"
+tables_dir = "PATH_TO_FOLDER/"
 
 murders %>% write.table(. , file = file.path(tables_dir, "murders.tsv"), 
                         sep = '\t', dec = ".", row.names = F, quote = F)
@@ -28,4 +29,7 @@ heights %>% write.table(. , file = file.path(tables_dir, "heights.tsv"),
                         sep = '\t', dec = ".", row.names = F, quote = F)
 
 divorce_margarine %>% write.table(. , file = file.path(tables_dir, "divorce_margarine.tsv"), 
+                                  sep = '\t', dec = ".", row.names = F, quote = F)
+
+ukbabynames %>% write.table(. , file = file.path(tables_dir, "ukbabynames.tsv"), 
                                   sep = '\t', dec = ".", row.names = F, quote = F)
